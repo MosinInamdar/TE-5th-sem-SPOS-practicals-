68554a2bc2c9789a115dac0e87af77bd168eca94
Mosin Inamdar <100260513+MosinInamdar@users.noreply.github.com>
2023-08-30 09:47:24 +0530

    Add files via upload

---
+++ B1.c
@@ -0,0 +1,25 @@
+#include <jni.h>
+#include <stdio.h>
+#include "B1.h"
+JNIEXPORT int JNICALL Java_B1_add(JNIEnv *env, jobject obj, jint a, jint b)
+{
+    printf("\n%d +%d = %d\n",a,b,(a+b));
+    return 0;
+}
+JNIEXPORT int JNICALL Java_B1_sub(JNIEnv *env, jobject obj,jint a,jint b)
+{
+    printf("\n%d - %d = %d\n",a,b,(a-b));
+    return 0;
+}
+
+JNIEXPORT int JNICALL Java_B1_mult(JNIEnv *env, jobject obj,jint a,jint b)
+{
+    printf("\n%d * %d = %d\n",a,b,(a*b));
+    return 0;
+}
+
+JNIEXPORT int JNICALL Java_B1_div(JNIEnv *env, jobject obj,jint a,jint b)
+{
+    printf("\n%d / %d = %d\n",a,b,(a/b));
+    return 0;
+}
